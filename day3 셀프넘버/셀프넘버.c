d3753c393a20aa1d764ad0ac3420f42b098fd73a
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-11-23 16:38:05 +0900

    Add files via upload

---
+++ day3 셀프넘버/셀프넘버.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int d(int n);
+int list[10000];
+
+int main(void)
+{	
+	int i;
+	for(i=0; i<10000; i++)
+	{
+		list[d(i)] = 1;
+		if(!list[i]) printf("%d\n", i);	
+	}
+		
+	return 0;
+}
+
+int d(int n)
+{
+	int i, sum=n;
+	for(i=0; i<4; i++)
+	{
+		sum += n%10;
+		n /= 10;
+	}
+	return sum;
+}
