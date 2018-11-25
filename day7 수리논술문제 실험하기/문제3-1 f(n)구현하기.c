25fdf6f97432bca52040a6527c4b04808c54a2fc
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-11-25 20:45:43 +0900

    Add files via upload

---
+++ day7 수리논술문제 실험하기/문제3-1 f(n)구현하기.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[35][35], i, j, n, cnt=1, result, Is_break=0;
+	
+	for(i=0; i<35; i++){
+		for(j=0; j<35; j++){
+			a[i][j] = cnt;
+			cnt++;
+		}
+	}
+	
+	printf("n : ");
+	
+	scanf("%d", &n);
+	
+	
+	cnt = 1;
+	
+	for(i=0; i<2*35-1; i++){
+		if(i<35){
+			for(j=i; j>=0; j--){
+				if(cnt==n){
+					result = a[j][i-j];
+					Is_break = 1;
+					break;
+				}
+				cnt++;
+			}
+			if(Is_break){
+				break;
+			}
+		}
+		else{
+			for(j=34; j>i-35; j--){
+				if(cnt==n){
+					result = a[j][i-j];
+					Is_break = 1;
+					break;
+				}
+				cnt++;
+			}
+			if(Is_break){
+				break;
+			}
+		}
+	}
+	
+	printf("f(%d) = %d", n, result);
+	
+	
+	return 0;
+}
