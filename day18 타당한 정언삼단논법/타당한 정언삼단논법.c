4a24701b2079e0b8612b065dbe29d1ddc1b74795
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-12-16 21:52:54 +0900

    Update 타당한 정언삼단논법.c

--- day18 타당한 정언삼단논법/타당한 정언삼단논법.c
+++ day18 타당한 정언삼단논법/타당한 정언삼단논법.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct{
-	int quatifier;		//양화사 : 모든/어떤 
-	int subject_term;	//주어개념 
-	int predicate_term;	//술어개념 
-	int copula;		//계사 : 이다/아니다 
-}Categorical_Proposition;	//정언명제 구조체 
+	bool subject_distribution;	//주연:true 
+	bool predicate_distribution;	//부주연:false 
+}Categorical_Proposition;
+
+typedef struct{
+	Categorical_Proposition major_premise;	//대전제 
+	Categorical_Proposition minor_premise;	//소전제 
+	Categorical_Proposition conclusion;	//결론
+	int figure;				//격식 
+}syllogism;
+
+
+Categorical_Proposition A = {true, false};	//전칭긍정명제 
+Categorical_Proposition E = {true, true};	//전칭부정명제 
+Categorical_Proposition I = {false, false};	//특칭긍정명제 
+Categorical_Proposition O = {false, true};	//특칭부정명제 
+
+
+
 
 int main(void){ 
+	int i;
+	
+	printf("논증 식 : ");
+
+	
+	
 	
 	return 0;
 }
