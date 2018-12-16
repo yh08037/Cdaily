f0656579450efeec231b06c6b7b14e5f49a5957f
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-12-16 18:05:02 +0900

    Update and rename tmp.txt to 타당한 정언삼단논법.c

---
+++ day18 타당한 정언삼단논법/타당한 정언삼단논법.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct{
+	int quatifier;		    	//양화사 : 모든/어떤 
+	int subject_term;		    //주어개념 
+	int predicate_term;		  //술어개념 
+	int copula;				      //계사 : 이다/아니다 
+}Categorical_Proposition;	//정언명제 구조체 
+
+int main(void){ 
+	
+	return 0;
+}
