b1977315023e0c6deb4ade7a0ef10be52788f648
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-12-22 04:01:44 +0900

    Update 타당한 정언삼단논법.c

--- day18 타당한 정언삼단논법/타당한 정언삼단논법.c
+++ day18 타당한 정언삼단논법/타당한 정언삼단논법.c
@@ -26,16 +26,54 @@ bool UndistributedMiddle(Syllogism*);		//매개념 부주연의 오류
 bool IllicitMajor(Syllogism*);			//대개념 부당주연의 오류 
 bool IllicitMinor(Syllogism*);			//소개념 부장주연의 오류 
 bool ExclusivePremises(Syllogism*);		//양부정전제의 오류 
-bool AffirmativeFromNegative(Syllogism*);	//부당긍정의 오류 
+bool IllicitAffirmation(Syllogism*);		//부당긍정의 오류 
 bool ExistentialFallacy(Syllogism*);		//존재가정의 오류 
 
 
 int main(void){
+	/*
 	Syllogism input;
 	
 	printf("논증 식 : ");
 	
 	GetSyllogism(&input);
+	*/
+	
+	int i, j, k, l, m, cnt, cntValid=0;
+	char temp[] = "AEIO";
+	Syllogism input;
+	bool fallacyTable[6]; 
+	
+	for(i=0; i<4; i++){
+		input.majorPremise = SetProposition(temp[i]);		
+		for(j=0; j<4; j++){
+			input.minorPremise = SetProposition(temp[j]);
+			for(k=0; k<4; k++){
+				input.conclusion = SetProposition(temp[k]);
+				for(l=1; l<=4; l++){
+					cnt = 0;
+					input.figure = l;
+					fallacyTable[0] = UndistributedMiddle(&input);
+					fallacyTable[1] = IllicitMajor(&input);
+					fallacyTable[2] = IllicitMinor(&input);
+					fallacyTable[3] = ExclusivePremises(&input);
+					fallacyTable[4] = IllicitAffirmation(&input);
+					fallacyTable[5] = ExistentialFallacy(&input);
+					printf("%c%c%c-%d   ", temp[i], temp[j], temp[k], l);
+					for(m=0; m<6; m++){
+						printf("%d ", fallacyTable[m]);
+						cnt += fallacyTable[m]?1:0;
+					}
+					if(cnt==0){
+						printf("  VALID");
+						cntValid++;
+						printf(" %d", cntValid);
+					}
+					printf("\n");
+				}
+			}
+		}
+	}
 	
 	return 0;
 }
@@ -59,22 +97,96 @@ CategoricalProposition SetProposition(char chr){
 
 /*매개념 부주연의 오류*/
 bool UndistributedMiddle(Syllogism* syllogism){
-	switch (syllogism->figure){
+	switch(syllogism->figure){
 		case 1:
+			if(!(syllogism->majorPremise.subjectDistribution)){
+				if(!(syllogism->minorPremise.predicateDistribution)){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
 		case 2:
+			if(!(syllogism->majorPremise.predicateDistribution)){
+				if(!(syllogism->minorPremise.predicateDistribution)){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
 		case 3:
+			if(!(syllogism->majorPremise.subjectDistribution)){
+				if(!(syllogism->minorPremise.subjectDistribution)){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
 		case 4:
+			if(!(syllogism->majorPremise.predicateDistribution)){
+				if(!(syllogism->minorPremise.subjectDistribution)){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
 	}
 } 
 
 /*대개념 부당주연의 오류*/
 bool IllicitMajor(Syllogism* syllogism){
-	
+	switch (syllogism->figure){
+		case 1:
+		case 3:
+			if(!(syllogism->majorPremise.predicateDistribution)){
+				if(syllogism->conclusion.predicateDistribution){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
+		case 2:
+		case 4:
+			if(!(syllogism->majorPremise.subjectDistribution)){
+				if(syllogism->conclusion.predicateDistribution){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
+	}
 }
 
 /*소개념 부당주연의 오류*/
 bool IllicitMinor(Syllogism* syllogism){
-	
+	switch (syllogism->figure){
+		case 1:
+		case 2:
+			if(!(syllogism->minorPremise.subjectDistribution)){
+				if(syllogism->conclusion.subjectDistribution){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
+		case 3:
+		case 4:
+			if(!(syllogism->minorPremise.predicateDistribution)){
+				if(syllogism->conclusion.subjectDistribution){
+					return true;
+				}
+				else return false;
+			}
+			else return false;
+			break;
+	}
 }
 
 /*양부정전제의 오류*/
@@ -89,7 +201,7 @@ bool ExclusivePremises(Syllogism* syllogism){
 }
 
 /*부당긍정의 오류*/
-bool AffirmativeFromNegative(Syllogism* syllogism){
+bool IllicitAffirmation(Syllogism* syllogism){
 	if(syllogism->majorPremise.predicateDistribution||syllogism->minorPremise.predicateDistribution){
 		if(!(syllogism->conclusion.predicateDistribution)){
 			return true;
